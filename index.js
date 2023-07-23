63885082067b02a448935e29cea987482c6599cd
ma-graff <140285532+ma-graff@users.noreply.github.com>
2023-07-22 20:40:35 -0700

    uploaded index.html, styles.css, index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+// Function to be executed when the page loads
+function onPageLoad() {
+    alert("Welcome to my website!");
+  }
+  
+  // Function to be executed when the button is clicked
+//   function onButtonClick() {
+//     var button = document.getElementById("myButton");
+//     button.textContent = "Button Clicked!";
+//   }
+  
+  // Attach the onPageLoad function to the 'load' event of the window object
+//   window.addEventListener("load", onPageLoad);
